9308bb32e882a80d6c1b4a02aef017eace579d5d
katabame <ayame@kataba.me>
2024-04-23 02:34:32 +0900

    upload

--- build-hyprland-ubuntu.sh
+++ build-hyprland-ubuntu.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -ex
 
+# For Ubuntu 22.04
+UBUNTU_VERSION=`lsb_release -r | awk '{print $2}'`
+OLD_UBUNTU_VERSION='22.04'
+PIXMAN_VERSION='0.43.4'
+XCBPROTO_VERSION='1.17.0'
+LIBXCB_VERSION='1.17.0'
+
 HYPRLAND_VERSION='0.38.1'
 HYPRLANG_VERSION='0.5.0'
 HYPRCURSOR_VERSION='0.1.7'
@@ -9,17 +16,37 @@ WAYLAND_PROTOCOLS_VERSION='1.34'
 LIBDISPLAY_INFO_VERSION='0.1.1'
 TOMLPLUSPLUS_VERSION='3.4.0'
 
-sudo apt-get install -y --no-install-recommends \
-    meson wget build-essential ninja-build cmake-extras cmake \
-    gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev \
-    libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev \
-    libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev \
-    vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 \
-    libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev \
-    libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev \
-    libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev \
-    libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr hwdata \
-    libcairo2-dev libzip-dev librsvg2-dev libgbm-dev jq
+if [ "${UBUNTU_VERSION}" = "${OLD_UBUNTU_VERSION}" ]; then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get install -y --no-install-recommends \
+        wget build-essential cmake-extras cmake \
+        gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev \
+        libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev \
+        libudev-dev libseat-dev seatd libvulkan-dev libvulkan-volk-dev \
+        vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 \
+        libegl1-mesa-dev glslang-tools libinput-bin libinput-dev \
+        libavutil-dev libavcodec-dev libavformat-dev \
+        libpango1.0-dev xdg-desktop-portal-wlr hwdata \
+        libcairo2-dev libzip-dev librsvg2-dev libgbm-dev jq \
+        gcc-13 g++-13 python3-pip autoconf automake xutils-dev libtool \
+        xwayland libliftoff-dev
+    sudo pip3 install meson ninja
+    export CC=/usr/bin/gcc-13
+    export CXX=/usr/bin/g++-13
+else
+    sudo apt-get install -y --no-install-recommends \
+        meson wget build-essential ninja-build cmake-extras cmake \
+        gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev \
+        libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev \
+        libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev \
+        vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 \
+        libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev \
+        libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev \
+        libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev \
+        libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr hwdata \
+        libcairo2-dev libzip-dev librsvg2-dev libgbm-dev jq \
+        xwayland libliftoff-dev
+fi
 
 mkdir ~/HyprSource
 cd ~/HyprSource
@@ -73,6 +100,26 @@ wget -O libdrm.tar.gz "https://gitlab.freedesktop.org/mesa/drm/-/archive/main/dr
 mkdir ~/HyprSource/libdrm &&
 tar -xzf libdrm.tar.gz -C ~/HyprSource/libdrm --strip-components 1 &&
 rm libdrm.tar.gz
+
+if [ "${UBUNTU_VERSION}" = "${OLD_UBUNTU_VERSION}" ]; then
+    # pixman
+    wget -O pixman.tar.gz "https://cairographics.org/releases/pixman-${PIXMAN_VERSION}.tar.gz"
+    mkdir ~/HyprSource/pixman &&
+    tar -xzf pixman.tar.gz -C ~/HyprSource/pixman --strip-components 1 &&
+    rm pixman.tar.gz
+
+    # xcb-proto
+    wget -O xcb-proto.tar.gz "https://xcb.freedesktop.org/dist/xcb-proto-${XCBPROTO_VERSION}.tar.gz"
+    mkdir ~/HyprSource/xcb-proto &&
+    tar -xzf xcb-proto.tar.gz -C ~/HyprSource/xcb-proto --strip-components 1 &&
+    rm xcb-proto.tar.gz
+
+    # libxcb
+    wget -O libxcb.tar.gz "https://xcb.freedesktop.org/dist/libxcb-${LIBXCB_VERSION}.tar.gz"
+    mkdir ~/HyprSource/libxcb &&
+    tar -xzf libxcb.tar.gz -C ~/HyprSource/libxcb --strip-components 1 &&
+    rm libxcb.tar.gz
+fi
 
 ## Build dependencies
 
@@ -130,6 +177,30 @@ cd ~/HyprSource/hyprcursor
 cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
 cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF`
 sudo cmake --install ./build
+
+if [ "${UBUNTU_VERSION}" = "${OLD_UBUNTU_VERSION}" ]; then
+    # pixman
+    cd ~/HyprSource/pixman
+    mkdir ./build &&
+    cd    ./build &&
+    meson setup --prefix=/usr --buildtype=release &&
+    ninja
+    sudo ninja install
+
+    # xcb-proto
+    cd ~/HyprSource/xcb-proto
+    ./autogen.sh
+    ./configure &&
+    make
+    sudo make install
+
+    # libxcb
+    cd ~/HyprSource/libxcb
+    ./autogen.sh
+    ./configure &&
+    make
+    sudo make install
+fi
 
 # hyprland
 cd ~/HyprSource/hyprland
